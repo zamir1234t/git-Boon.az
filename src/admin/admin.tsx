0a8983f6b220261dd4de4f560eea03ece2a4c05d
zamir1234t <firamogurova579@gmail.com>
2025-05-23 20:30:32 +0400

    boon.az

--- src/admin/admin.tsx
+++ src/admin/admin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import Register from '../components/Register';
 import { Outlet, Link } from 'react-router-dom';
 import { useCart } from '../context/greatecontext';
